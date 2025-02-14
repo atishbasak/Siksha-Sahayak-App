26545bc3fae43b4ac0ff6bd73946671e802ec309
AtishBasak <atishbasak24@gmail.com>
2025-02-15 05:08:53 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    packaging{
+        resources.excludes.add("META-INF/NOTICE.md")
+        resources.excludes.add("META-INF/LICENSE.md")
+    }
 }
 
 dependencies {
@@ -40,6 +45,13 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("com.github.smarteist:autoimageslider:1.4.0")
+    implementation("com.sun.mail:android-mail:1.6.6")
+    implementation("com.sun.mail:android-activation:1.6.7")
+    implementation ("com.squareup.picasso:picasso:2.8")
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation ("com.google.android.gms:play-services-auth:20.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/sikshasahayak/HomePage.java
+++ app/src/main/java/com/example/sikshasahayak/HomePage.java
@@ -1,6 +1,8 @@
 package com.example.sikshasahayak;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,8 @@ import com.smarteist.autoimageslider.SliderView;
 
 
 public class HomePage extends AppCompatActivity {
+
+    public static  final String FILTER = "ccom.example.foode_commerceapp.extra.FILTER";
 
     SliderView sliderView;
     int[] images = {R.drawable.eduone,
@@ -44,7 +48,120 @@ public class HomePage extends AppCompatActivity {
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
 
-
-
     }
+
+//    public void toFilterPageBiriyani(View view) {
+//        String item = "biriyani";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPagePizza(View view) {
+//        String item = "pizza";
+//        boolean id = true;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageChicken(View view) {
+//        String item = "chicken";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageChiliChicken(View view) {
+//        String item = "chiliChicken";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageEggRoll(View view) {
+//        String item = "eggRoll";
+//        boolean id = true;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageCake(View view) {
+//        String item = "cake";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPagePaneer(View view) {
+//        String item = "paneer";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageBurger(View view) {
+//        String item = "burger";
+//        boolean id = true;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageSandwich(View view) {
+//        String item = "sandwich";
+//        boolean id = true;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageChinese(View view) {
+//        String item = "chinese";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageManchurian(View view) {
+//        String item = "manchurian";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageColdDrinks(View view) {
+//        String item = "coldDrinks";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageWeeklyTop(View view) {
+//        String item = "weeklyTop";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+//    public void toFilterPageRating(View view) {
+//        String item = "rating";
+//        boolean id = false;
+//        Intent intent = new Intent(this, FilterItemsPage.class);
+//        intent.putExtra(FILTER,item);
+////        intent.putExtra(DISH_NAME,id);
+//        startActivity(intent);
+//    }
+
+
 }
